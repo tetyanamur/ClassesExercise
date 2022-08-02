04e2a8dc5fadb60ee2420bbb464ed8ce0d271d3d
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-02 16:52:49 -0500

    initial commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class Car
+    {  //constructors 
+
+       public Car()
+       {
+
+        }
+        public Car(string make, string model, int year)
+            {
+            Make = make;
+            Model = model;
+            Year = year;
+            }
+        //fields
+        //properties
+        //methods
+        public string Make { get; set; } //get = read; set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Mercedes";
+            myCar.Model = "Benze";
+            myCar.Year = 2019;     //or:
+
+            var honda = new Car()
+            {
+                Make = "Honda",
+                Model = "civic",
+                Year = 2014
+            };
+            var saturn = new Car("Saturn", "Vue", 2005);
+            var carList = new List<Car>() { myCar, honda, saturn };
+            foreach (var vechile in carList)
+            {
+                Console.WriteLine($"{vechile.Make} {vechile.Model} {vechile.Year}");
+            }
+        }
+
+    }
+}
